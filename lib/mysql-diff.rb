859ecfc896143fcacedf80f90e7cea05daeb3e16
Bradley Atkins <atkinsb@atkinsb-MacBook-Pro.local>
2016-03-04 16:28:57 +0000

    refactored delta refs

--- lib/mysql-diff.rb
+++ lib/mysql-diff.rb
@@ -1,7 +1,7 @@
 
 # A table delta. Holds all data required to resolve a db delta
 # DB Schema is assumed
-class MysqlDelta
+class MysqlDiff
 
   attr_accessor :deltas, :is_delta
 
@@ -10,7 +10,7 @@ class MysqlDelta
     @is_delta = false
   end
 
-  def test_delta(arg1, arg2, type)
+  def test_diff(arg1, arg2, type)
     unless arg1 == arg2
       @deltas[type.to_sym] = [arg1, arg2]
       @is_delta = true
